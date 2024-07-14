8696c969d2537b24a10f1a10904388b804fd9222
Pratik3311 <152001676+Pratik3311@users.noreply.github.com>
2024-07-15 02:26:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://todo-frontend-9wz1.onrender.com/',
+    origin: 'https://todo-frontend-9wz1.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
