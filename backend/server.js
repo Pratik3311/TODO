4a31232e61743fe066beb46c2f894c4d42148ffe
Pratik3311 <152001676+Pratik3311@users.noreply.github.com>
2024-07-15 01:36:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin : [""],
+    methods :["POST","GET"],
+    credentials : true
+  }
+));
 app.use(bodyParser.json());
 
 // MongoDB Connection
